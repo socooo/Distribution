94921dab80b01b32d304a990d0c79fe0a8e97788
Chao Song <chao.song@leapstack.cn>
2019-03-23 05:29:50 +0800

    Lab 1 done.

--- src/mapreduce/schedule.go
+++ src/mapreduce/schedule.go
@@ -1,6 +1,11 @@
 package mapreduce
 
-import "fmt"
+import (
+	"fmt"
+	"sync"
+	"log"
+	"time"
+)
 
 //
 // schedule() starts and waits for all tasks in the given phase (Map
@@ -32,5 +37,52 @@ func schedule(jobName string, mapFiles []string, nReduce int, phase jobPhase, re
 	//
 	// TODO TODO TODO TODO TODO TODO TODO TODO TODO TODO TODO TODO TODO
 	//
+	workersGroup := sync.WaitGroup{}
+	workersGroup.Add(ntasks)
+
+	for taskp:=0; taskp < ntasks; taskp++{
+		go func(taskNo int, phase jobPhase, wg *sync.WaitGroup) {
+			var callOk bool
+			for{
+				worker, tchanOk := <- registerChan
+				if !tchanOk {
+					log.Fatal("Error when read registerChan")
+				}
+				switch phase {
+				case mapPhase:
+					doTaskArg := DoTaskArgs{
+						jobName,
+						mapFiles[taskNo],
+						phase,
+						taskNo,
+						n_other}
+					callOk = call(worker, "Worker.DoTask", doTaskArg, new(struct{}))
+				case reducePhase:
+					doTaskArg := DoTaskArgs{
+						jobName,
+						"",
+						phase,
+						taskNo,
+						n_other}
+					callOk = call(worker, "Worker.DoTask", doTaskArg, new(struct{}))
+				}
+				if !callOk{
+					time.Sleep(time.Second)
+				}
+				if callOk {
+					go func(worker string){		// 启动新线程 将 worker 加入到 registerChan 中，否则最后一个执行task的线程会被阻塞。
+						registerChan <- worker
+					}(worker)
+					break
+				} else {
+					go func(worker string){		// 启动新线程 将 worker 加入到 registerChan 中，否则最后一个执行task的线程会被阻塞。
+						registerChan <- worker
+					}(worker)
+				}
+			}
+			defer wg.Done()
+		}(taskp, phase, &workersGroup)
+	}
+	workersGroup.Wait()
 	fmt.Printf("Schedule: %v phase done\n", phase)
 }
